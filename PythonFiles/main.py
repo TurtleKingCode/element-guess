fb123a5a18c9e8bd9689fcd924cdc901a567b3fc
Franklin Oguama <turtle.foguama@gmail.com>
2020-10-31 21:30:08 +0000

    Clyde can now pick random numbers

--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -6,11 +6,18 @@ import sys
 from time import sleep
 
 # Setting up Print function
-def Print (words):
+def Print (words, speed=0.01):
 	for char in words:
-		sleep(0.01)
+		sleep(speed)
 		sys.stdout.write(char)
 		sys.stdout.flush()
+
+# Setting up random pick function
+def pick_random_element():
+	global randomElement
+	randomElement = random.choice(Elem.Elements)
+	if (randomElement.number == 0):
+		pick_random_element()
 
 # Setting up Clyde
 Bot = Responces.Character("Clyde", "I like computing numbers,\nplaying games,\nrunning new code programs,\nand Watching Mecha Anime.\n(My favorite is Robotics;Notes)")
@@ -46,12 +53,12 @@ RULES
 2. You will have to guess the Atomic Number of that Element (ranging from 1 to 118).
 3. ''' + Bot.name + ''' will tell you whether the number is too small or too large.
 4. If you guess a number you already guessed, ''' + Bot.name + ''' will notify you on that.  But he will still count that as an attempt.
-4. The process repeats until you guess the number.
-5. Or until ''' + Bot.name + ''' gets too tired and tells you to stop.
-6. As you know, ''' + Bot.name + ''' doesn\'t have all day.
-7. Meaning if you can't guess correctly after 60 tries, ''' + Bot.name + ''' will give up on you and reveal the answer.
-8. Finally, ''' + Bot.name + ''' will give you your score based on your performances.
-9. And you can decided to play again or stop playing the game entirely.
+5. The process repeats until you guess the number.
+6. Or until ''' + Bot.name + ''' gets too tired and tells you to stop.
+7. As you know, ''' + Bot.name + ''' doesn\'t have all day.
+8. Meaning if you can't guess correctly after 60 tries, ''' + Bot.name + ''' will give up on you and reveal the answer.
+9. Finally, ''' + Bot.name + ''' will give you your score based on your performances.
+10. And you can decided to play again or stop playing the game entirely.
 ___________________________________________________
 
 Press Enter to Start: ''')
@@ -61,6 +68,6 @@ Start = input()
 Print('''
 Aight, let's start!!!
 Give me some time to guess an Element...
-[''' + Bot.name + ''' goes brrrr]
-........................................................................................................................................................................................................................................................................................................''')
-randomElement = random.choice(Elem.Elements)
+[''' + Bot.name + ''' goes brrrr]''')
+Print('''
+.........................................................................................................................................................................................''', 0.02)
--- PythonFiles/test.py
+++ PythonFiles/test.py
@@ -1,6 +1,6 @@
-import Elem
-import Responces
-import main
-import random
-import sys
-from time import sleep
+# import Elem
+# import Responces
+# import main
+# import random
+# import sys
+# from time import sleep
