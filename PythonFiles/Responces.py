de1daebee64606ce6be5b285d37be966501e4b47
Franklin Oguama <turtle.foguama@gmail.com>
2020-10-31 16:43:06 +0000

    Clyde introduces himself.

--- PythonFiles/Responces.py
+++ PythonFiles/Responces.py
@@ -1,5 +1,24 @@
+import random
+
 class Character:
 	def __init__(self, name, description):
 		self.name = name
 		self.description = description
-		self.intro = "Hi, my name is " + self.name + ", " + self.description + "."
+		self.intro = "Hi, my name is " + self.name + "\n\n" + self.description + "."
+
+
+class nameAdjs:
+	def __init__(self):
+		self.first = ["cool", "awesome"]
+		self.second = ["splendiferous", "fantastical", "outstanding", "awesome", "fantabulous", "radical"]
+		self.chooseFirst = random.choice(self.first)
+		self.chooseSecond = random.choice(self.second)
+
+nameAdjs = nameAdjs()
+
+class Respond:
+	def askName(self):
+		print("Anyways, what " + nameAdjs.chooseFirst + "name do you want me to call you?")
+	class userName:
+		def __init__(self):
+			self.userName = str(input("My " + nameAdjs.chooseSecond + " name is: "))
--- PythonFiles/main.py
+++ PythonFiles/main.py
@@ -4,11 +4,37 @@ import Responces
 import random
 
 # Setting up Clyde
-Clyde = Responces.Character("Clyde", "I like computing numbers, playing games, running new code programs, and Watching Mecha Anime. (My favorite is Robotics;Notes)")
+Clyde = Responces.Character("Clyde", "I like computing numbers,\nplaying games,\nrunning new code programs,\nand Watching Mecha Anime.\n(My favorite is Robotics;Notes)")
 
+# Setting up Responces
+Respond = Responces.Respond()
+
+# Starting the Talking
 print(Clyde.intro)
-# print (x)
 
+print("\n--------------------------------------\n")
+
+Respond.askName()
+userName = Respond.userName().userName
+
+# print("")
+# print ("Hi " + userName + ", nice to meet you!!!")
+# print("And")
+# print("WELCOME!!! To my...")
+# print("Number Guessing Game of the Elements of the Periodic Table")
+# print("---------------------------------------")
+print('''
+Hi ''' + userName + ''', nice to meet you!!
+And
+WELCOME!!! To my...
+Number Guessing Game of the Elements of the Periodic Table 
+---------------------------------------
+''')
+# print("")
+# print(Respond.userName().username)
+
+# print(type(Respond.userName()))
+# print(Respond.userName() + "Yes")
 
 # print(Elem.Elements[0].name)
 # print (Responces.Intro)
